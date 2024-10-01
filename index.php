a32509b12adb5a54351dd83c8b5c97fb3d53ff76
Viktor-TPD <viktortohverstridh@gmail.com>
2024-10-01 15:53:08 +0200

    'index.php' prints out info about the teams and their logo with correct alt

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
-<!-- THIS IS WHERE MY LANDING PAGE GOES :) -->
+<!DOCTYPE html>
+<html lang="en">
+    <!-- THIS IS WHERE MY LANDING PAGE GOES :) -->
 
 <!-- 16 LAG; PRESENTERA DEM 3x PER SIDA? BÖRJAR MED DET SOM AMBITION. -->
 
 <!-- EXEMPEL PÅ ENTRY I $TEAMS: -->
 
+<!-- skicka till header.php > -->
+<!-- $teams[] -->
 <!-- 'SKN St. Pölten' => [
         'league' => 'ÖFB-Frauenliga (Austria)',
         'uefa-coefficient-ranking' => 26,
@@ -12,19 +16,25 @@
         'logo' => 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSkiSy4BkhZp1RgOL8cKly0rIU2uSXshWGSzQ&s',
         'group' => 'D'
     ] -->
-<!-- skicka till header.php > -->
-<!DOCTYPE html>
-<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>🏃‍♀️⚽Some Teams⚽🏃‍♀️</title>
+    <!-- PHP STUFFS -->
+    <?php include_once __DIR__ . '/data.php'; ?>
 </head>
 <!-- ^ skicka till header.php -->
 <!-- include_once __DIR__ . "header.php"; --> <!-- lägg till php tags här! -->
 <body>
-    <!-- börja med att bara visa namnen på nya rader -->
-    
+    <?php foreach ($teams as $team => $teamName):
+    ?><article>
+        <img src="<?= $teamName['logo']; ?>" alt="<?= $team ?>'s logo.">
+        <h1><?= $team;?></h1>
+
+
+    </article>
+    <?php endforeach; ?>
+
 </body>
 </html>
 
