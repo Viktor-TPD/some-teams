9513af97551a918acb04bfb30864333a7e199af1
Viktor-TPD <viktortohverstridh@gmail.com>
2024-10-04 13:15:34 +0200

    Added a backup stylesheet

--- index2.php
+++ index2.php
@@ -1,15 +1,18 @@
 <!-- header goes here -->
 <?php include_once __DIR__ . "/header.php"; ?>
 <body>
-    
-<div class="logo_container">
-    <?php foreach ($teams as $team => $teamName):
+ <div class="master_container">
+
+     <div class="logo_container">
+         <?php foreach ($teams as $team => $teamName):
         ?> 
         <div>
             <img src="<?= $teamName['logo']; ?>" alt="<?= $team ?>'s logo.">
         </div>
-    <?php endforeach; ?>
-</div>
+        <?php endforeach; ?>
+    </div>
+    <div class="article_container">TEST TEST TEST</div>
+</div>   
 
 <!-- footer goes here -->
 <?php include_once __DIR__ . "/footer.php"; ?>
